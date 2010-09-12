278d19229317e3c980ee74249d05a8a33f853e23
jenny <jenny@adaptivel.(none)>
2010-09-12 01:06:42 -0500

    still learning =) fixed my models so they worked, at least for creating db tables

--- challenge/models.py
+++ challenge/models.py
@@ -2,30 +2,30 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
-    title = models.CharField
-    price = models.DecimalField
-    location = models.CharField
+    title = models.CharField(max_length=256)
+    price = models.FloatField()
+    location = models.CharField(max_length=256)
 
 class ItemContact(models.Model):
     item = models.ForeignKey(Item)
-    email = models.CharField
-    phone = models.CharField
-    name = models.CharField
-    fb_profile = models.CharField
+    email = models.CharField(max_length=64)
+    phone = models.CharField(max_length=16)
+    name = models.CharField(max_length=64)
+    fb_profile = models.CharField(max_length=64)
 
 class ItemDescription(models.Model):
     item = models.ForeignKey(Item)
-    description = models.CharField
+    description = models.CharField(max_length=1024)
 
 class ItemPicture(models.Model):
     item = models.ForeignKey(Item)
-    picture = models.CharField
+    picture = models.CharField(max_length=256)
 
 class Person(models.Model):
-    username = models.CharField
-    location = models.CharField
+    username = models.CharField(max_length=64)
+    location = models.CharField(max_length=255)
 
 class PersonFlags(models.Model):
     person = models.ForeignKey(Person)
     item = models.ForeignKey(Item)
-    flag = models.CharField
+    flag = models.CharField(max_length=64)
