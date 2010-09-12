20cb5eb5c76c58ec4398c2df559534dcc229b52f
Jennifer Lin <jennifer@webs.com>
2010-09-11 21:54:04 -0700

    initial models

--- challenge/models.py
+++ challenge/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    title = models.CharField
+    price = models.DecimalField
+    location = models.CharField
+
+class ItemContact(models.Model):
+    item = models.ForeignKey(Item)
+    email = models.CharField
+    phone = models.CharField
+    name = models.CharField
+    fb_profile = models.CharField
+
+class ItemDescription(models.Model):
+    item = models.ForeignKey(Item)
+    description = models.CharField
+
+class ItemPicture(models.Model):
+    item = models.ForeignKey(Item)
+    picture = models.CharField
+
+class Person(models.Model):
+    username = models.CharField
+    location = models.CharField
+
+class PersonFlags(models.Model):
+    person = models.ForeignKey(Person)
+    item = models.ForeignKey(Item)
+    flag = models.CharField
