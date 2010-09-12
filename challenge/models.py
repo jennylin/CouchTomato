184b59570261bc4044f57fe5f570bb98a2d23e76
jennylin <jlin@alumni.cmu.edu>
2010-09-12 10:38:20 -0500

    i think i'm returning results properly now

--- challenge/models.py
+++ challenge/models.py
@@ -7,7 +7,7 @@ class Item(models.Model):
     location = models.CharField(max_length=256)
     url = models.CharField(max_length=2048)
     def __unicode__(self):
-        return "$" + str(self.price) + " - " + self.title
+        return "{'title':'"+self.title+"', 'price':'"+str(self.price)+"', 'location':'"+self.location+"', 'url':'"+self.url+"'}"
 
 class ItemContact(models.Model):
     item = models.ForeignKey(Item)
@@ -18,7 +18,7 @@ class ItemContact(models.Model):
 
 class ItemDescription(models.Model):
     item = models.ForeignKey(Item)
-    description = models.CharField(max_length=2048)
+    description = models.CharField(max_length=32000)
 
 class ItemPicture(models.Model):
     item = models.ForeignKey(Item)
--- challenge/views.py
+++ challenge/views.py
@@ -3,14 +3,15 @@ from django.shortcuts import get_object_or_404, render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
+from django.core import serializers
 from CouchTomato.craigSearch import CraigsSearch
 
-def search(request, location):
-    cs = CraigsSearch(request.POST['term'], location)
+def search(request, location, term):
+    cs = CraigsSearch(term, location)
     results = cs.search()
-    return HttpResponseRedirect(reverse('/', args=(results)))
+    return render_to_response('index.htm', serializers.serialize("json",results))
 
-def search_category(request, location, category):
-    cs = CraigSearch(request.POST['term'], location)
+def search_category(request, category, location, term):
+    cs = CraigSearch(term, location)
     results = cs.searchCategory(category)
-    return HttpResponseRedirect(reverse('/', args=(results)))
+    return render_to_response('index.htm', serializers.serialize("json",results))
