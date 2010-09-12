de5b81eeacbe151777e752d47af99ffac57a16d0
Gonzalo Maldonado <gueb7@adaptivel.(none)>
2010-09-12 03:29:59 -0500

    Front page comming along nicely

--- urls.py
+++ urls.py
@@ -1,16 +1,18 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('django.views.generic.simple',
+urlpatterns = patterns('',
+    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
     # Example:
     #(r'^challenge/', include('CouchTomato.urls')),
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^$','direct_to_template', {'template': 'index.htm'}),
+    (r'^$','django.views.generic.simple.direct_to_template', {'template': 'index.htm'}),
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 )
