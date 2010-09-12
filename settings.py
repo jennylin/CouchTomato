9e64c35179af8a71ab76a6f58557c29e7dca4764
Gonzalo Maldonado <gueb7@adaptivel.(none)>
2010-09-12 01:30:14 -0500

    Missed the templates dir

--- settings.py
+++ settings.py
@@ -9,10 +9,10 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_ENGINE = 'postgresql_psycopg2'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
+DATABASE_NAME = 'geoBS'       # Or path to database file if using sqlite3.
+DATABASE_USER = 'adaptLS'             # Not used with sqlite3.
+DATABASE_PASSWORD = '7n7f7wyI7rW!11'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
