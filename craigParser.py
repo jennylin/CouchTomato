b97a3605f18280646f3da693c09353f9028eb1ee
jennylin <jlin@alumni.cmu.edu>
2010-09-12 05:46:48 -0500

    more stuff but description, pictures still not saving and contact info still not being parsed

--- craigParser.py
+++ craigParser.py
@@ -1,24 +1,37 @@
 from BeautifulSoup import BeautifulSoup
+from challenge.models import ItemPicture
 import re
 import urllib 
 
 class CraigsParser:
     """ Awesome functions to scrape data from CraigsList"""
     def __init__(self, url):
-    	self.url = url
+    	craigDoc = urllib.urlopen(url)
+    	self.craigSoup = BeautifulSoup(craigDoc)
     		
+    def getPrice(self):
+        """ Gets the title from the post returns str """
+    	price = self.craigSoup.html.body.h2.contents[0].lstrip(" ")
+        try:
+            price = price.split(" - ")[1]
+            price = price.split(" ")[0]
+        except IndexError, e:
+            return "-1" # means item is not for sale
+        return price
     def getTitle(self):
         """ Gets the title from the post returns str """
-    	craigDoc = urllib.urlopen(self.url)
-    	craigSoup = BeautifulSoup(craigDoc)
-    	title = craigSoup.html.body.h2.prettify()
+    	title = self.craigSoup.html.body.h2.contents[0].split(" - ")[0]
     	return title
     def getContent(self):
         """ Gets the content from the post returns str """
-    	craigDoc = urllib.urlopen(self.url)
-    	craigSoup = BeautifulSoup(craigDoc)
-        userbody = craigSoup.findAll('div', id="userbody")
+        userbody = self.craigSoup.findAll('div', id="userbody")
         userbodySTR = userbody[0].prettify() # May not be necessary refactor.
         content = userbodySTR.split("<!--")[0] #could use something more elegant...
     	content = re.sub('<div id="userbody">', '', content) #specially this.
     	return content
+    def getPictures(self, item):
+        pictures = self.craigSoup.findAll("img")
+        for pic in pictures:
+            item_pic = ItemPicture()
+            item_pic.item = item
+            item_pic.pic = pic
